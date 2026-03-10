d75cb8deba4787a7f331c2321ee6cc496acfc2ef
Imalsha-Ranasinghe <imalsharanasinghe2020@gmail.com>
2026-03-10 23:40:40 +0530

    Convolution Kernel

--- src/mpi/convolution_mpi.c
+++ src/mpi/convolution_mpi.c
@@ -4,6 +4,36 @@
 #include <mpi.h>
 #include "../../include/image_utils.h"
 
+unsigned char apply_kernel(Image *img, int x, int y, int c, float *kernel, int ksize) {
+
+    int half = ksize / 2;
+    float sum = 0;
+
+    for (int ky = -half; ky <= half; ky++) {
+        for (int kx = -half; kx <= half; kx++) {
+
+            int img_x = x + kx;
+            int img_y = y + ky;
+
+            if (img_x < 0) img_x = 0;
+            if (img_x >= img->width) img_x = img->width - 1;
+            if (img_y < 0) img_y = 0;
+            if (img_y >= img->height) img_y = img->height - 1;
+
+            int img_index = (img_y * img->width + img_x) * img->channels + c;
+            int kernel_index = (ky + half) * ksize + (kx + half);
+
+            sum += img->data[img_index] * kernel[kernel_index];
+        }
+    }
+
+    if (sum < 0) sum = 0;
+    if (sum > 255) sum = 255;
+
+    return (unsigned char)sum;
+}
+
+
 int main(int argc, char *argv[]) {
 
     MPI_Init(&argc, &argv);
